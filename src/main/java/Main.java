5ebfceca5c718ce0d9608c8e8c1837dde9be0cbc
José Gabriel Morais <169068802+moraisdino@users.noreply.github.com>
2024-09-02 12:02:40 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+  //Um programa que leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida. Mostre quantos destes valores X estão dentro do intervalo[10,20] e quanto estão fora do intervalo, mostrando essas informações;
+
+    int QuantidadeDeNumerosDigitados = sc.nextInt();
+    int ValoresDentroDoIntervalo = 0; 
+    int ValoresForaDoIntervalo = 0;
+    int NumeroDigitado;
+
+    for(int i = 0; i <) {
+
+
+
+      
+    }
+    
+    
+    
+
+    sc.close();
+  }
+}
